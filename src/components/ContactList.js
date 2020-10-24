8887506064464393da217ea22b47cf29465b1e64
kardelen-karatas <kardelen.karatas@gmail.com>
2020-10-24 17:03:48 +0200

    iteration 2

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,20 +3,35 @@ import contacts from '../contacts.json';
 
 class ContactList extends React.Component {
   state = {
-    contacts: contacts,
+    cnts: [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]],
   };
+
+  addRandomContact = () => {
+    let num = Math.floor(Math.random() * Math.floor(contacts.length));
+
+    if(!this.state.cnts.includes(contacts[num])){
+        this.setState({
+            cnts:[
+                ...this.state.cnts,
+                contacts[num]
+            ]
+        })
+    } else console.log(`${contacts[num]} is already in the table`)
+  }
 
   render() {
     console.log(contacts);
     return (
       <div className="contact-list">
+
+        <button onClick={this.addRandomContact}> Add Random Contact</button>
         <tr className="contact">
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
         </tr>
 
-        {this.state.contacts.map((contact) => (
+        {this.state.cnts.map((contact) => (
           <tr key={contact.id}>
             <td>
               <img src={contact.pictureUrl} alt=""></img>
